6a3ac64484a4a70c3c33fbea55bb57734d9e67a4
abdallah <abddoullah.bouzhar@gmail.com>
2023-04-20 18:59:26 +0000

    added the palette colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,16 @@ export default {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+
     extend: {
+      colors: {
+        'darkBlue': '#0B2545',
+        'lightBlue': '#00C6BD',
+        'yellow': '#FFF7D6',
+        'lightGray': '#E2F0FF',
+        'darkGray': '#B2C2ED',
+        'yellow': '#ffc82c',
+      },
       backgroundImage: {
         'pitch': "url('https://res.cloudinary.com/doy8hfzvk/image/upload/v1681842404/afb9101577d749b22e01ff13d32f5f9e_ykgxhl.jpg')",
         
