59f8a6a14a34a07b9d17eca8f32e1e7fbebf6e45
abdallah <abddoullah.bouzhar@gmail.com>
2023-04-23 20:23:25 +0100

    home fantazy

--- frontend/src/components/fantazy/CreateSquad.jsx
+++ frontend/src/components/fantazy/CreateSquad.jsx
@@ -17,12 +17,9 @@ import InputName from "../elements/InputName";
 import ReviewConfirmSquad from "../elements/ReviewConfirmSquad";
 
 export default function CreateSquad() {
-  const { name, isLoading, logoURL, logos, hasStarted, step } =
-    useSelector((state) => state.squad);
-
-    
-
-    
+  const { name, isLoading, logoURL, logos, hasStarted, step } = useSelector(
+    (state) => state.squad
+  );
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -32,49 +29,51 @@ export default function CreateSquad() {
   if (!hasStarted) {
     return (
       <>
-        <div className="bg-blue-300 p-3 mx-auto flex items-center">
-          <h2 className="text-xl text-center">
+        <div className="dark:bg-violet-400">
+          <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
+            <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-900">
             Create your squad, and join the game now!
-          </h2>
-          <div>
-            <Button
-              className="mx-5"
-              outline={true}
-              gradientDuoTone="purpleToBlue"
-              onClick={() => dispatch(setStarted())}
-            >
-              let's go
-            </Button>
+            </h1>
+            <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">
+              You need to create a squad to join the botolaPro fantazy
+            
+            </p>
+            <div className="flex flex-wrap justify-center">
+              <button
+                type="button"
+                onClick={() => dispatch(setStarted())}
+                className="bg-gradient-to-r from-gradient2 to-gradient1 px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-gray-800 dark:text-gray-50"
+              >
+                Get started
+              </button>
+
+            
+            </div>
           </div>
         </div>
-        <h1>section</h1>
+
       </>
     );
   }
 
   return (
     <>
-       
       {step < 3 && (
-
         <div className="flex justify-center items-end fixed bottom-0 w-full py-2 gap-2">
-        <Button onClick={() => dispatch(previousStep())}>prev</Button>
-        <Button onClick={() => dispatch(nextStep())}>next</Button>
-      </div>
-        )}
+          <Button onClick={() => dispatch(previousStep())}>prev</Button>
+          <Button onClick={() => dispatch(nextStep())}>next</Button>
+        </div>
+      )}
 
       <ProgressComp />
-          {step == 3 && <ReviewConfirmSquad />}
+      {step == 3 && <ReviewConfirmSquad />}
       <div className="grid grid-cols-3 justify-between mx-5 gap-5">
         <div className="col-span-2">
           {step == 2 && <PlayersC />}
           {step == 1 && <InputName />}
           {step == 0 && <Logos />}
         </div>
-        <div className="">
-          {step < 3 && <SquadInfo />}
-          
-        </div>
+        <div className="">{step < 3 && <SquadInfo />}</div>
       </div>
     </>
   );
--- frontend/src/components/fantazy/Home.jsx
+++ frontend/src/components/fantazy/Home.jsx
@@ -7,12 +7,32 @@ import { Link } from "react-router-dom";
 import { decryptData } from "../../utilities/functions";
 import { getCompetition } from "../../features/fantazy/competitionSlice";
 export default function Home() {
- 
-
   return (
     <>
-      <HomeHeader path="Home"/>
-<h1>home</h1>
+      <HomeHeader path="Home" />
+      <section>
+        <div className="dark:bg-violet-400">
+          <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
+            <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-900">
+              Welcome to BotolaPro Fantazy
+            </h1>
+            <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">
+              select 11 players for each week and collect as much points as you can !
+            </p>
+            <div className="flex flex-wrap justify-center">
+            
+              
+              <Link
+              className="bg-gradient-to-r from-gradient2 to-gradient1  px-8 py-3 m-2 text-lg border rounded dark:border-gray-700 dark:text-gray-900"
+              to="/selection"
+            >
+              Week Selection
+            </Link>
+            </div>
+          </div>
+        </div>
+      
+      </section>
     </>
   );
 }
